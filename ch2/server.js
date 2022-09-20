2e6ac9db15e562953d5c98846d4f6359eed67ef8
Gabriel M. Lujan <gfl.wrk@gmail.com>
2022-09-19 17:11:37 -0700

    feat(main): add stock server values & NPM script;

--- ch2/server.js
+++ ch2/server.js
@@ -1,1 +1,13 @@
 const ws = new WebSocket('ws://localhost:8123');
+
+const stockRequest = {
+   stocks: ['AAPL', 'MSFT', 'AMZN', 'GOOG', 'YHOO'],
+};
+
+const stocks = {
+   AAPL: 0,
+   MSFT: 0,
+   AMZN: 0,
+   GOOG: 0,
+   YHOO: 0,
+};
